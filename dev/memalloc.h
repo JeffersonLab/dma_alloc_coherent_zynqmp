8b190c88ff1f67fb1b02802ae13ba0263f74d579
Bryan Moffit <moffit@jlab.org>
2018-07-11 13:34:15 -0400

    Add space for virtual address.. so we can use the same structure in both spaces. Remove the c++ ifdef in header.

--- dev/memalloc.h
+++ dev/memalloc.h
@@ -1,10 +1,6 @@
 #ifndef MEMALLOC_H
 #define MEMALLOC_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <linux/types.h>
 #include <asm/ioctl.h>
 
@@ -15,19 +11,17 @@ extern "C" {
 #define MEMALLOC_BUFFER_MAX_NUMBER 16
 
 #define MEMALLOC_IOCTL_BASE 1
-#define MEMALLOC_RESERVE_CMD         _IO(MEMALLOC_IOCTL_BASE, 0) 
+#define MEMALLOC_RESERVE_CMD         _IO(MEMALLOC_IOCTL_BASE, 0)
 #define MEMALLOC_RELEASE_CMD         _IO(MEMALLOC_IOCTL_BASE, 1)
 #define MEMALLOC_GET_PHYSICAL_CMD    _IO(MEMALLOC_IOCTL_BASE, 2)
 #define MEMALLOC_ACTIVATE_BUFFER_CMD _IO(MEMALLOC_IOCTL_BASE, 3)
 
 typedef struct ioctl_arg_t
 {
-	size_t buffer_size;      /* in */
-	int buffer_id;           /* in, out */
-	unsigned long phys_addr; /* out */
+  size_t buffer_size;		/* in */
+  int buffer_id;		/* in, out */
+  unsigned long phys_addr;	/* out */
+  unsigned long virt_addr;	/* userspace (mmap result) */
 } ioctl_arg_t;
 
-#ifdef __cplusplus
-}
-#endif
-#endif /* MEMALLOC_H */
+#endif				/* MEMALLOC_H */
